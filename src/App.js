9887b42d445b89eafd31ba787e6bf924b172a869
Sunny So <sohiutung123456@yahoo.com.hk>
2023-03-21 15:34:32 +0800

    First commit for first-react-project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Checkout from './Checkout';
+import ProductDetail from './ProductDetail';
+import ProductList from './ProductList';
+import { CartContext } from './CartContext';
+import { useState } from 'react';
+import Error from './Error';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [cartItems, setCartItems] = useState([]);
+
+	return (
+		<BrowserRouter>
+
+            <CartContext.Provider value={{cartItems,setCartItems}}>
+
+                <nav>
+                    <Link to="/">首頁</Link> 
+                    <Link to="/checkout">購物車</Link>
+                </nav>
+
+                <Routes>
+                    <Route path="/" element={<ProductList/>} />
+                    <Route path="/checkout" element={<Checkout/>} />
+
+                    <Route path="/product" element={<ProductDetail/>}>
+                        <Route path=":id" element={<ProductDetail/>} />
+                    </Route>
+
+                    <Route path="*" element={<Error/>}/>
+                </Routes>
+
+            </CartContext.Provider>
+
+	    </BrowserRouter>
+	);
 }
 
 export default App;
---
+++ src/QuantityBtn.js
@@ -0,0 +1,83 @@
+import { CartContext } from "./CartContext"
+import { useContext, useState } from "react"
+
+export default function QuantityBtn({product}) {
+
+    const {cartItems, setCartItems} = useContext(CartContext)
+
+    //購物車內有冇該產品
+    let productIndexInCart = cartItems.findIndex((element)=>{
+        return element.id === product.id
+    })
+    //findIndex()
+    //如果係購物車內找到該件產品 => 返回索引位置 0, 1, 2, 3.....
+    //該件產品沒有被加入過去購物車 => 返回 -1
+
+    let [numInCart, setNumInCart] = useState(
+        (productIndexInCart === -1) ? 0 : cartItems[productIndexInCart].quantity
+    )
+
+    const handleAdd = ()=>{
+
+        if(productIndexInCart === -1)
+        {
+            //購物車本身沒有，在cartItems array中加個新element (object)
+            setCartItems(
+                [
+                    {
+                        id : product.id,
+                        name : product.name,
+                        image : product.image,
+                        price : product.price,
+                        description : product.description,
+                        quantity : 1
+                    },
+                    ...cartItems
+                ]
+            )
+        }
+        else
+        {
+            //購物車有該產品，只加個quantity
+            let newCartArray = [...cartItems]
+            newCartArray[productIndexInCart].quantity++
+            setCartItems(newCartArray)
+        }
+
+        setNumInCart(numInCart+1)
+    }
+
+    const handleSubtract = ()=>{
+
+        if(cartItems[productIndexInCart].quantity===1)
+        {
+            //購在物車中只剩一件的話，remove object
+            let newCartArray = [...cartItems]
+            newCartArray.splice(productIndexInCart, 1)
+            setCartItems(newCartArray)
+        }
+        else
+        {
+            //只減個quantity
+            let newCartArray = [...cartItems]
+            newCartArray[productIndexInCart].quantity--
+            setCartItems(newCartArray)
+        }
+
+        setNumInCart(numInCart-1)
+    }
+
+    return (
+        <div className="addToCart">
+            {
+                (numInCart === 0) ?
+                <span className="addToCartBtn" onClick={handleAdd}>加入購物車</span> :
+                <div>
+                    <span className="subtractBtn" onClick={handleSubtract}>-</span>
+                    {numInCart}件
+                    <span className="addBtn" onClick={handleAdd}>+</span>
+                </div>
+            }
+        </div>
+    )
+}
